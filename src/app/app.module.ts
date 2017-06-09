6756a679935c4bbfdf1337a5a0ddeb2fbb8e8811
Jerome Joestar <marcus.ty.parker@gmail.com>
2017-06-09 18:06:18 -0400

    Initialize app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FilterBarComponent } from './filter-bar/filter-bar.component';
+import { SearchComponent } from './filter-bar/search/search.component';
+import { FilterComponent } from './filter-bar/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FilterBarComponent,
+    SearchComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
